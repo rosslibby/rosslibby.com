d40218a5279647d452bb36aaa1d993351f8a7b5f
Ross Libby <rosslibby92@gmail.com>
2025-08-04 12:40:34 -0400

    set up typedefs for project

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+import { Dispatch, SetStateAction } from 'react';
+
+export interface ViewfinderCtx {
+  insights: TargetInsight[];
+  viewfinder: boolean;
+  targetSpecs: TargetSpecs;
+  _: Record<string, Dispatch<SetStateAction<any>>>;
+}
+
+export type TargetSpecs = {
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+};
+
+export type TargetInsight = {
+  code: string;
+  explanation?: string;
+};
+
+export interface CursorCtx {
+  coordinates: [number, number];
+  down: boolean;
+  targeting: string | null;
+  _: Record<string, Dispatch<SetStateAction<any>>>;
+}
