205fe3689a1422e69560cc40a8cef07ff0f2ce34
Ross Libby <rosslibby92@gmail.com>
2025-08-04 12:41:15 -0400

    implemented contexts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ViewfinderProvider } from "@/viewfinder-context";
+import { CursorProvider } from "@/cursor-context";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,9 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <CursorProvider>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          
+          <ViewfinderProvider>
+            {children}
+          </ViewfinderProvider>
+        </body>
+      </CursorProvider>
     </html>
   );
 }
