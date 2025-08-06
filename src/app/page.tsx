d5d3aeeadeb2feb9de0cba20a5d144c4d3f9ac2f
Ross Libby <rosslibby92@gmail.com>
2025-08-06 14:28:21 -0400

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Cursor, Drawer } from '@/components';
+import { Drawer, Target, ViewFinder } from '@/components';
 import styles from './page.module.scss';
 
 export default function Home() {
@@ -8,11 +8,48 @@ export default function Home() {
     <>
       <Drawer />
       <main className={styles.main}>
-        <h1 data-target="true" data-target-id="0" data-component-id="intro">Hey, I'm <span>a Typescript developer</span>.</h1>
-        <button data-target="true" data-target-id="1" data-component-id="counter">Click counter (<span id="counter"></span>)</button>
+        <Target id="0" insights={[
+          {
+            code: 'const { noun } = useIntro();',
+            explanation: 'Get the "noun" variable from the useIntro() hook',
+          },
+          { code: '' },
+          { code: 'return (' },
+          {
+            code: '  <h1>Hey, I\'m a <span>{noun}</span>.</h1>',
+            explanation: 'Display a heading that introduces me as the noun we got from the useIntro() hook',
+          },
+          { code: ');' },
+        ]}>
+          <h1 data-target="true" data-target-id="0" data-component-id="intro">Hey, I'm <span>a Typescript developer</span>.</h1>
+        </Target>
+        <Target id="1" insights={[
+          {
+            code: 'const [count, setCount] = useState(0);',
+            explanation: 'Set a variable to track a count starting from 0',
+          },
+          {
+            code: 'const label = `Click counter (${count})`;',
+            explanation: 'Set the button\'s text to display its title and the current count',
+          },
+          { code: '' },
+          {
+            code: 'const handleClick = () => setCount((count) => count + 1);',
+            explanation: 'Build a function that will increment the counter',
+          },
+          { code: '' },
+          { code: 'return (' },
+          {
+            code: '  <button onClick={handleClick}>{label}</button>',
+            explanation: 'Display a button that shows the button\'s label and increments the count when clicked',
+          },
+          { code: ');' },
+        ]}>
+          <button data-target="true" data-target-id="1" data-component-id="counter">Click counter (<span id="counter"></span>)</button>
+        </Target>
       </main>
 
-      <Cursor />
+      <ViewFinder />
     </>
   );
 }
