56f83345878f49cc6507a98d281ce9e77622c0a2
Ross Libby <rosslibby92@gmail.com>
2025-08-11 16:10:36 -0400

    remove drawer component; implement Counter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,50 @@
 'use client';
 
-import { Drawer, Intro, Target } from '@/components';
+import { Counter, Intro, Target } from '@/components';
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <>
-      <Drawer />
-      <main className={styles.main}>
-        <Target id="intro" insights={[
-          {
-            code: 'const { noun } = useIntro();',
-            explanation: 'Get the "noun" variable from the useIntro() hook',
-          },
-          { code: '' },
-          { code: 'return (' },
-          {
-            code: '  <h1>Hey, I\'m a <span>{noun}</span>.</h1>',
-            explanation: 'Display a heading that introduces me as the noun we got from the useIntro() hook',
-          },
-          { code: ');' },
-        ]}>
-          <Intro />
-        </Target>
-        <Target id="counter" insights={[
-          {
-            code: 'const [count, setCount] = useState(0);',
-            explanation: 'Set a variable to track a count starting from 0',
-          },
-          {
-            code: 'const label = `Click counter (${count})`;',
-            explanation: 'Set the button\'s text to display its title and the current count',
-          },
-          { code: '' },
-          {
-            code: 'const handleClick = () => setCount((count) => count + 1);',
-            explanation: 'Build a function that will increment the counter',
-          },
-          { code: '' },
-          { code: 'return (' },
-          {
-            code: '  <button onClick={handleClick}>{label}</button>',
-            explanation: 'Display a button that shows the button\'s label and increments the count when clicked',
-          },
-          { code: ');' },
-        ]}>
-          <button data-target="true" data-target-id="counter">Click counter (<span></span>)</button>
-        </Target>
-      </main>
-    </>
+    <main className={styles.main}>
+      <Target id="intro" insights={[
+        {
+          code: 'const { noun } = useIntro();',
+          explanation: 'Get the "noun" variable from the useIntro() hook',
+        },
+        { code: '' },
+        { code: 'return (' },
+        {
+          code: '  <h1>Hey, I\'m <span>{noun}</span>.</h1>',
+          explanation: 'Display a heading that introduces me as the noun we got from the useIntro() hook',
+        },
+        { code: ');' },
+      ]}>
+        <Intro />
+      </Target>
+      <Target id="counter" insights={[
+        {
+          code: 'const [count, setCount] = useState(0);',
+          explanation: 'Set a variable to track a count starting from 0',
+        },
+        {
+          code: 'const label = `Click counter (${count})`;',
+          explanation: 'Set the button\'s text to display its title and the current count',
+        },
+        { code: '' },
+        {
+          code: 'const handleClick = () => setCount((count) => count + 1);',
+          explanation: 'Build a function that will increment the counter',
+        },
+        { code: '' },
+        { code: 'return (' },
+        {
+          code: '  <button onClick={handleClick}>{label}</button>',
+          explanation: 'Display a button that shows the button\'s label and increments the count when clicked',
+        },
+        { code: ');' },
+      ]}>
+        <Counter />
+      </Target>
+    </main>
   );
 }
