302ed134017cede622ab12bf38bf08a239e55a4f
Ross Libby <rosslibby92@gmail.com>
2025-08-12 15:35:32 -0400

    dynamic width animations

--- src/components/intro/hooks.tsx
+++ src/components/intro/hooks.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useCallback, useEffect, useState } from 'react';
+import styles from './intro.module.scss';
 
 const nouns = [
   'Ross',
@@ -16,11 +17,19 @@ const nouns = [
   'a React developer',
 ];
 
-export const useIntro = () => {
+type useIntroProps = {
+  targetId: string;
+  manualCycle?: boolean;
+};
+export const useIntro = ({
+  targetId,
+  manualCycle,
+}: useIntroProps) => {
   const [history, setHistory] = useState<string[]>([nouns[0]]);
   const [noun, setNoun] = useState(nouns[0]);
+  const [dimensions, setDimensions] = useState<[number, number]>([0, 0]);
 
-  const getRandomNoun = useCallback(() => {
+  const getRandomNoun = useCallback(async () => {
     const list = nouns.filter((noun) => !history.includes(noun));
     if (!list.length) {
       setHistory([nouns[0]]);
@@ -29,22 +38,68 @@ export const useIntro = () => {
 
     const index = Math.floor(Math.random() * list.length);
     const noun = list[index];
+    await preRender(noun);
     setHistory((prev) => [...prev, noun]);
     setNoun(noun);
   }, [history, setHistory, setNoun]);
 
+  const preRender = useCallback(async (noun: string) => {
+    const bounds = await prerender(targetId, noun);
+    setDimensions([bounds.width, bounds.height]);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 350);
+    });
+  }, [targetId, setDimensions]);
+
   useEffect(() => {
-    let timer: NodeJS.Timeout | null = setInterval(() => {
-      getRandomNoun();
-    }, 3600);
+    const cycle = (e: KeyboardEvent) => {
+      if (e.key === ' ') {
+        getRandomNoun();
+      }
+    };
+    let timer: NodeJS.Timeout | null
+
+    if (manualCycle) {
+      document.addEventListener('keydown', cycle);
+    } else {
+      timer = setInterval(() => {
+        getRandomNoun();
+      }, 3600);
+    }
 
     return () => {
       if (timer) {
         clearInterval(timer);
         timer = null;
       }
-    };
-  }, [noun]);
 
-  return { noun };
+      if (manualCycle) {
+        document.removeEventListener('keydown', cycle);
+      }
+    };
+  }, [manualCycle, noun]);
+
+  return { dimensions, noun };
 };
+
+async function prerender(targetId: string, noun: string): Promise<DOMRect> {
+  const target = document.querySelector(
+    `[data-target-id="${targetId}"]`
+  ) as HTMLDivElement;
+  const clone = target.cloneNode(true) as HTMLDivElement;
+  clone.querySelector('h1')?.removeAttribute('style');
+  clone.querySelector('span')!.textContent = noun;
+  clone.classList.add(styles.prerender);
+  clone.setAttribute('data-clone-id', targetId);
+  clone.style.width = 'max-content';
+  (target.parentElement as HTMLElement).insertBefore(clone, target);
+  return new Promise((resolve) => {
+    const bounds = clone.getBoundingClientRect();
+    setTimeout(() => {
+      clone.remove();
+      resolve(bounds);
+    });
+  });
+}
--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -6,10 +6,18 @@ import styles from './intro.module.scss';
 export * from './hooks';
 
 export const Intro = () => {
-  const { noun } = useIntro();
+  const { dimensions: [width, height], noun } = useIntro({
+    targetId: 'intro',
+    manualCycle: false,
+  });
+
+  const style = {
+    height: height ? `${height}px` : 'auto',
+    width: width ? `${width}px` : 'auto',
+  } as React.CSSProperties;
 
   return (
-    <h1 className={styles.heading}>
+    <h1 className={styles.heading} style={style}>
       Hey, I'm <span className={styles.noun}>{noun}</span>.
     </h1>
   );
