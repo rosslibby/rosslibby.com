75a61b05c1c6ac3829444d85848c50ed79100275
Ross Libby <rosslibby92@gmail.com>
2025-08-12 14:48:28 -0400

    implement drawer dismiss via outside click

--- src/components/drawer/hooks/drawer.tsx
+++ src/components/drawer/hooks/drawer.tsx
@@ -1,10 +1,39 @@
-'use client';
+'use client'
 
-import { useContext } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import { cursorCtx } from '@/cursor-context';
 
-export const useDrawer = () => {
-  const { targeting } = useContext(cursorCtx);
+export const useDrawer = (
+  drawerRef: React.RefObject<HTMLDivElement | null>,
+) => {
+  const { targeting, _: { setTargeting } } = useContext(cursorCtx);
+
+  const handleOutsideClick = useCallback((e: MouseEvent) => {
+    const current = drawerRef.current as HTMLDivElement | null;
+    const target = e.target as HTMLElement;
+
+    if (!current || targeting === null) return;
+    else console.log('click detected:', current, target, current.contains(target))
+
+    if (!current.contains(target)) {
+      setTargeting(null);
+    }
+  }, [drawerRef, targeting, setTargeting]);
+
+  useEffect(() => {
+    let current = drawerRef.current as HTMLDivElement | null;
+
+    if (!current) return;
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      if (current) {
+        document.removeEventListener('mousedown', handleOutsideClick);
+        current = null;
+      }
+    };
+  }, [drawerRef, handleOutsideClick]);
 
   return { targeting };
 };
--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -1,19 +1,25 @@
-'use client';
+'use client'
 
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { cursorCtx } from '@/cursor-context';
 import { Code } from './code';
 import { Component } from './component';
+import { useDrawer } from './hooks';
 import styles from './drawer.module.scss';
 
 export const Drawer = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const { targeting } = useContext(cursorCtx);
+  useDrawer(containerRef);
+
   const attributes = {
+    className: styles.drawer,
+    ref: containerRef,
     ...(targeting ? { 'data-targeting': targeting } : {}),
   };
 
   return (
-    <div className={styles.drawer} {...attributes}>
+    <div {...attributes}>
       <Component />
       <Code />
     </div>
