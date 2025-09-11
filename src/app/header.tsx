375041f14d7f1075c6f82488c82fc844be90f0f8
Ross Libby <rosslibby92@gmail.com>
2025-09-11 18:11:27 -0400

    oraganization

---
+++ src/app/header.tsx
@@ -0,0 +1,8 @@
+import { Intro } from '@/components';
+import styles from './page.module.scss';
+
+export const Header = () => (
+  <div className={styles.header}>
+    <Intro />
+  </div>
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,14 @@
-'use client'
-
-import Image from 'next/image';
-import Link from 'next/link';
-import { Block, Blocks, BlockTitle, FeatureDemo, Intro, NpmPackages } from '@/components';
+import { Blocks, BlockTitle, FeatureDemo, NpmPackages } from '@/components';
+import { Header } from './header';
+import { Hero } from './hero';
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.header}>
-        <Intro />
-      </div>
+      <Header />
       <hr />
-      <Blocks>
-        <BlockTitle
-          title="Fullstack Node/Typescript Maven"
-          subtitle="From vision to production, I build software that lasts. I thrive on creating scalable systems and impactful user experiences that stand the test of time."
-        />
-      </Blocks>
-      <Blocks columns="repeat(3, 1fr)" style={{ maxWidth: '40rem', zIndex: 2 }}>
-        <Block style={{ borderLeft: 'none', justifyContent: 'end', alignItems: 'center', gap: '0.5rem' }}>
-          <Link href="https://github.com/rosslibby" target="_blank">
-            <Image src="github-mark.svg" width={32} height={32} alt="GitHub" />
-          </Link>
-          <p>
-            <Link href="https://github.com/rosslibby" target="_blank">rosslibby</Link>
-          </p>
-          <h3 style={{fontSize: '14px', fontWeight: 700}}>GitHub</h3>
-        </Block>
-        <Block style={{ justifyContent: 'center', alignItems: 'center', gap: '0.75rem' }}>
-          <Link href="https://www.linkedin.com/in/rosslibby" target="_blank">
-            <Image src="/InBug-White.png" width={32} height={32} alt="LinkedIn" />
-          </Link>
-          <p>
-            <Link href="https://www.linkedin.com/in/rosslibby" target="_blank">in/rosslibby</Link>
-          </p>
-          <h3 style={{fontSize: '14px', fontWeight: 700}}>LinkedIn</h3>
-        </Block>
-        <Block style={{ borderRight: 'none', justifyContent: 'start', alignItems: 'center', gap: '0.75rem' }}>
-          <Link href="https://www.npmjs.com/~rosslibby" target="_blank">
-            <Image src="Npm-logo.svg" width={82.28} height={32} alt="LinkedIn" />
-          </Link>
-          <p>
-            <Link href="https://www.npmjs.com/~rosslibby" target="_blank">~rosslibby</Link>
-          </p>
-          <h3 style={{fontSize: '14px', fontWeight: 700}}>NPM</h3>
-        </Block>
-      </Blocks>
+      <Hero />
       <div><FeatureDemo /></div>
       <Blocks>
         <BlockTitle
