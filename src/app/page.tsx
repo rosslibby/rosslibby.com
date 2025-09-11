38e5642f3cd9f68cebdbf29bc89b62e98b77583e
Ross Libby <rosslibby92@gmail.com>
2025-09-11 17:58:24 -0400

    title links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,13 +21,13 @@ export default function Home() {
       </Blocks>
       <Blocks columns="repeat(3, 1fr)" style={{ maxWidth: '40rem', zIndex: 2 }}>
         <Block style={{ borderLeft: 'none', justifyContent: 'end', alignItems: 'center', gap: '0.5rem' }}>
-          {/* <h3>GitHub</h3> */}
           <Link href="https://github.com/rosslibby" target="_blank">
             <Image src="github-mark.svg" width={32} height={32} alt="GitHub" />
           </Link>
           <p>
             <Link href="https://github.com/rosslibby" target="_blank">rosslibby</Link>
           </p>
+          <h3 style={{fontSize: '14px', fontWeight: 700}}>GitHub</h3>
         </Block>
         <Block style={{ justifyContent: 'center', alignItems: 'center', gap: '0.75rem' }}>
           {/* <h3>LinkedIn</h3> */}
@@ -37,6 +37,7 @@ export default function Home() {
           <p>
             <Link href="https://www.linkedin.com/in/rosslibby" target="_blank">in/rosslibby</Link>
           </p>
+          <h3 style={{fontSize: '14px', fontWeight: 700}}>LinkedIn</h3>
         </Block>
         <Block style={{ borderRight: 'none', justifyContent: 'start', alignItems: 'center', gap: '0.75rem' }}>
           {/* <h3>NPM</h3> */}
@@ -46,6 +47,7 @@ export default function Home() {
           <p>
             <Link href="https://www.npmjs.com/~rosslibby" target="_blank">~rosslibby</Link>
           </p>
+          <h3 style={{fontSize: '14px', fontWeight: 700}}>NPM</h3>
         </Block>
       </Blocks>
       <div><FeatureDemo /></div>
