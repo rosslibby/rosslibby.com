b1a8041368ed8e741463d1a3c60ae5a158bd36e0
Ross Libby <rosslibby92@gmail.com>
2025-09-11 16:35:19 -0400

    cleanup

--- src/components/code/index.tsx
+++ src/components/code/index.tsx
@@ -2,8 +2,6 @@
 import { useRef } from 'react';
 import { useBackgrounds, useCode } from './hooks';
 import styles from './code.module.scss';
-import feature from '../feature/feature.module.scss';
-import { Feature } from '../feature';
 
 export const Code = ({ code, featured, language, background, title }: {
   code: string;
@@ -24,28 +22,19 @@ export const Code = ({ code, featured, language, background, title }: {
   return (
     <div className={classname} data-theme="nuxt-contrast" style={style}>
       {changing && <div className={styles.fadebg} />}
-      {/* <Feature> */}
-      {/* <div className={feature._container}>
-        <div className={feature.background} />
-        <div className={feature.floating}>
-          <div className={feature.content}> */}
-            <div className={styles.frame}>
-              <div className={styles.header}>
-                <div className={styles.buttons}>
-                  <div className={styles.button}></div>
-                  <div className={styles.button}></div>
-                  <div className={styles.button}></div>
-                </div>
-                <div className={styles.title}>{title || 'index.ts'}</div>
-              </div>
-              <div className={styles.body}>
-                <div className={styles.code} ref={codeRef} />
-              </div>
-            </div>
-          {/* </div>
+      <div className={styles.frame}>
+        <div className={styles.header}>
+          <div className={styles.buttons}>
+            <div className={styles.button}></div>
+            <div className={styles.button}></div>
+            <div className={styles.button}></div>
+          </div>
+          <div className={styles.title}>{title || 'index.ts'}</div>
         </div>
-      </div> */}
-      {/* </Feature> */}
+        <div className={styles.body}>
+          <div className={styles.code} ref={codeRef} />
+        </div>
+      </div>
     </div>
   );
 };
