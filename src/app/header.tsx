02807276abbeac35e0d81db9600144dca84266a6
Ross Libby <rosslibby92@gmail.com>
2025-09-12 21:11:56 -0400

    these components were moved to src/components dir

--- src/app/header.tsx
+++
@@ -1,8 +0,0 @@
-import { Intro } from '@/components';
-import styles from './page.module.scss';
-
-export const Header = () => (
-  <header className={styles.header}>
-    <Intro />
-  </header>
-);
