78afe610f8246e39e687a616ec5894d0b1cad362
Ross Libby <rosslibby92@gmail.com>
2025-11-28 11:53:43 -0500

    organize page code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { useRef } from 'react';
-import { Block, Blocks, BlockTitle, FeatureDemo, NpmPackages } from '@/components';
-import { Header, Hero } from '@/components';
-import styles from './page.module.scss';
-import footerStyles from './footer.module.scss';
-import Image from 'next/image';
-import Link from 'next/link';
+import {
+  FeatureDemo,
+  Footer,
+  Header,
+  Hero,
+  NpmPackages,
+} from '@/components';
+import styles from './page.module.css';
 
 export default function Home() {
 
@@ -16,23 +17,8 @@ export default function Home() {
       <hr className="inset" />
       <Hero />
       <FeatureDemo />
-      <Blocks>
-        <BlockTitle
-          title="Open source contributions"
-          subtitle="Selected npm packages I've published and maintain"
-        />
-      </Blocks>
       <NpmPackages />
-      <footer className={footerStyles.footer}>
-        <Blocks>
-          <Block className={footerStyles.repo}>
-            <div style={{display: 'flex', gap: '0.75rem'}}>
-              <Image src="github-mark.svg" width={26} height={26} alt="Check out my website's source code" />
-              <p>Check out my website's <Link href="https://github.com/rosslibby/rosslibby.com" target="_blank">source code</Link></p>
-            </div>
-          </Block>
-        </Blocks>
-      </footer>
+      <Footer />
     </div>
   );
 }
