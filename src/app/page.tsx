f2346d108221d656307721850446106abb00cbcd
Ross Libby <rosslibby92@gmail.com>
2025-11-28 11:59:05 -0500

    propify Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,10 @@ export default function Home() {
     <div className={styles.layout}>
       <Header />
       <hr className="inset" />
-      <Hero />
+      <Hero
+        title="Fullstack Node/Typescript Maven"
+        subtitle="From vision to production, I build software that lasts. I thrive on creating scalable systems and impactful user experiences that stand the test of time."
+      />
       <FeatureDemo />
       <NpmPackages />
       <Footer />
